24667e84a0bf928321db23f550735e864ec9558b
kmje405 <kmje405@gmail.com>
2024-12-27 14:24:51 -0600

    commit CountButtons.jsx

--- src/components/CountButtons.jsx
+++ src/components/CountButtons.jsx
@@ -1,8 +1,9 @@
+
 export default function ChangeCount() {
-  return (
-    <div>
-      <button>+</button>
-      <button>-</button>
-    </div>
-  );
+    return (
+        <div>
+            <button>+</button>
+            <button>-</button>
+        </div>
+    )
 }
