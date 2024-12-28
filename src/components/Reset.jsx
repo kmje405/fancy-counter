f1e027d6a5a6811afc03b2edf3f42d6e89ac8ef0
kmje405 <kmje405@gmail.com>
2024-12-27 21:05:10 -0600

    add counter reset component

--- src/components/Reset.jsx
+++ src/components/Reset.jsx
@@ -1,3 +1,14 @@
-export default function Reset() {
-  return <button>Reset</button>;
+import { ResetIcon } from "@radix-ui/react-icons";
+
+export default function Reset({ setCount }) {
+  function handleCountReset() {
+    console.log("Reset button clicked");
+    setCount(0);
+  }
+
+  return (
+    <button className="reset-btn">
+      <ResetIcon onClick={handleCountReset} />
+    </button>
+  );
 }
