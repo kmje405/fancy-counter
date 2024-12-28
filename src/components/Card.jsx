876daddd07b009099af4ccb22e62a1477fa19f08
kmje405 <kmje405@gmail.com>
2024-12-27 21:05:53 -0600

    add card component

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,15 +1,21 @@
 import Title from "./Title";
 import Count from "./Count";
 import Reset from "./Reset";
-import CountButtons from "./CountButtons";
+import { useState } from "react";
+import ButtonContainer from "./ButtonContainer";
 
 export default function Card() {
+  const [count, setCount] = useState(() => {
+    console.log("Initial count state");
+    return 0;
+  });
+  console.log("Card rendered");
   return (
-    <div>
+    <div className="card">
       <Title />
-      <Count />
-      <Reset />
-      <CountButtons />
+      <Count count={count} />
+      <Reset setCount={setCount} />
+      <ButtonContainer setCount={setCount} />
     </div>
   );
 }
